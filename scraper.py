198b90043316692ef6badc3649d8d6a6b0d2c5d6
keyansheng <65121402+keyansheng@users.noreply.github.com>
2021-03-22 20:17:49 +0800

    feat: add scraper

---
+++ scraper.py
@@ -0,0 +1,16 @@
+import requests
+from bs4 import BeautifulSoup
+
+URL = "https://krdict.korean.go.kr/dicSearch/search?mainSearchWord={}"
+
+
+def generate_definition(query):
+    r = requests.get(URL.format(query))
+    soup = BeautifulSoup(r.content, "html.parser")
+    entries = soup.find_all("dl", class_="printArea")
+    results = []
+    for entry in entries:
+        word = entry.find("span", class_="word_type1_17").text
+        definition = entry.find("dd").text
+        results.append(" ".join(f"{word} = {definition}".split()))
+    return "<br>".join(results)
